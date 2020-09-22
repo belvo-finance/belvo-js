54e3872b57f88f9adbb81b8652ffad71b80942b6
maris <maristany.ramos.albert@gmail.com>
2020-09-22 09:39:23 +0200

    [Added] Add type to invoices endpoint

--- src/invoices.js
+++ src/invoices.js
@@ -19,11 +19,12 @@ class Invoice extends Resource {
    * @param {string} link UUID4 representation of a link Id.
    * @param {string} dateFrom - Required date from, format is YYYY-MM-DD.
    * @param {string} dateTo - Required date to, format is YYYY-MM-DD.
+   * @param {string} type - Required type, it can be 'INFLOW' or 'OUTFLOW'.
    * @param {object} options - Optional parameters (token, encryptionKey, saveData, attachXML)
    * @returns {object} Response
    * @throws {RequestError}
    */
-  async retrieve(link, dateFrom, dateTo, options = {}) {
+  async retrieve(link, dateFrom, dateTo, type, options = {}) {
     const {
       token, encryptionKey, saveData, attachXML,
     } = options;
